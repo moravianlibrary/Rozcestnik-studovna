c3ee9d6925868a370b71b9aa73573412b6ef1de9
Jiri Krepl <jiri.krepl@gmail.com>
2016-02-24 13:08:35 +0100

    homepage icons done, homepage events done

--- client/templates/homePage/helpCircle.js
+++ client/templates/homePage/helpCircle.js
@@ -1,5 +1,30 @@
 Template.helpCircle.events({
-    "click #circle-wrapper": function(event, template) {
+    "click .circle-wrapper": function(event, template) {
         Router.go(template.data);
+    },
+    "mouseenter .circle": function(event) {
+        var circle = $(event.target);
+        var text = circle.next();
+        circle.attr('class', 'circle circle-hover');
+        text.attr('class', 'circle-text circle-text-hover');
+    },
+    "mouseleave .circle": function(event) {
+        var circle = $(event.target);
+        var text = circle.next();
+        circle.attr('class', 'circle');
+        text.attr('class', 'circle-text');
+
+    },
+    "mouseenter .circle-text": function(event) {
+        var text = $(event.target);
+        var circle = text.prev();
+        circle.attr('class', 'circle circle-hover');
+        text.attr('class', 'circle-text circle-text-hover');
+    },
+    "mouseleave .circle-text": function(event) {
+        var text = $(event.target);
+        var circle = text.prev();
+        circle.attr('class', 'circle');
+        text.attr('class', 'circle-text');
     }
 });
