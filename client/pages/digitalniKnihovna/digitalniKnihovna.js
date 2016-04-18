212a981e28bbf935f8fa17c79b9f07850268dc1d
Jiri Krepl <jiri.krepl@gmail.com>
2016-04-18 09:32:57 +0200

    page digital library html+css done

--- client/pages/digitalniKnihovna/digitalniKnihovna.js
+++ client/pages/digitalniKnihovna/digitalniKnihovna.js
@@ -1,12 +1,5 @@
 Template.digitalniKnihovna.events({
-    "click #free-volumes": function () {
-        window.open('http://kramerius.mzk.cz/', '_blank');
-    },
-    "click #all-volumes": function () {
+    "click #all-volumes-button": function () {
         $('.alert').show();
-    },
-    "click .close": function (event) {
-        event.preventDefault()
-        console.log('dsdf');
     }
 });
