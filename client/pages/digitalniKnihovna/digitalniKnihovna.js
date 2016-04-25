1a77e55006bb699b5b2725e4f1fdef29c7b6e7d9
jirikrepl <jiri.krepl@gmail.com>
2016-04-25 13:31:12 +0200

    alert panel for all-volumes button

--- client/pages/digitalniKnihovna/digitalniKnihovna.js
+++ client/pages/digitalniKnihovna/digitalniKnihovna.js
@@ -1,5 +1,8 @@
 Template.digitalniKnihovna.events({
     "click #all-volumes-button": function () {
         $('.alert').show();
+        setTimeout(function () {
+            $('.alert').hide();
+        }, 5000);
     }
 });
